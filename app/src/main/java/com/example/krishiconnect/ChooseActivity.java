cd3250198585a37006fd271e90494536a30afdb7
jeewan aidi <aidizeon@gmail.com>
2024-12-13 11:52:00 +0545

    Splash Screen and Choose Activity

---
+++ app/src/main/java/com/example/krishiconnect/ChooseActivity.java
@@ -0,0 +1,59 @@
+package com.example.krishiconnect;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.krishiconnect.Customer.CustomerLoginActivity;
+import com.example.krishiconnect.Farmers.FarmerLoginActivity;
+import com.example.krishiconnect.Riders.RiderLoginActivity;
+
+public class ChooseActivity extends AppCompatActivity {
+
+    Button farmerBtn, customerBtn, riderBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_choose);
+
+        farmerBtn = findViewById(R.id.farmerBtn);
+        customerBtn = findViewById(R.id.customerBtn);
+        riderBtn = findViewById(R.id.riderBtn);
+
+        farmerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseActivity.this, FarmerLoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        customerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseActivity.this, CustomerLoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        riderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseActivity.this, RiderLoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/krishiconnect/SplashScreenActivity.java
+++ app/src/main/java/com/example/krishiconnect/SplashScreenActivity.java
@@ -1,12 +1,11 @@
 package com.example.krishiconnect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -14,11 +13,16 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, ChooseActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
